89cd222ba7f4ca860ab4197ba5839caf1e8025ec
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-23 03:26:15 +0800

    simplify

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 'use strict';
-var uniqueRandom = require('unique-random');
+var uniqueRandomArray = require('unique-random-array');
 var catNames = require('./cat-names.json');
-var catRandom = uniqueRandom(0, catNames.length - 1);
 
 exports.all = catNames;
-
-exports.random = function () {
-	return catNames[catRandom()];
-};
+exports.random = uniqueRandomArray(catNames);
