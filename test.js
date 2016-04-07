f02e0bbce6c3b0aa46a0c5543ceee671f8551aaa
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:20:34 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import x from './';
 
 test(t => {
 	t.true(x.all.length > 0);
-	t.ok(x.random());
+	t.truthy(x.random());
 	t.not(x.random(), x.random());
 	t.is(x.all[0], 'Abby');
 	t.is(x.all[1], 'Angel');
